5e7fa4cf3725fa81aa611e76980e87ad40b4b9f5
bmukankusi <b.mukankusi@alustudent.com>
2023-11-08 07:51:20 -0800

    Fix a bug

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,9 +2,10 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json". format(subreddit)
     headers = {
             "User-Agent": "Linux:api.advanced:vi.0.0 (by betty/mukankusi)"
             }
