760703dd830fb33bc4023bbcea528546359700cd
Betty Mukankusi <127393726+bmukankusi@users.noreply.github.com>
2023-11-09 23:13:29 +0300

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""get subscribers function"""
+
+
+import json
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json". format(subreddit)
-    headers = {"User-Agent": "Linux:api.advanced:v1.0.0 (by u/This_Dot_5148)"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """get all subscribers"""
+    if len(sys.argv) < 2:
         return 0
-    results = response.status_code.json().get("data")
-    return results.get("subscribers")
+    else:
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        headers = {"User-Agent": "Mozilla/5.0"}
+        result = requests.get(url, headers=headers, allow_redirects=False)
+        if result.status_code != 200:
+            return 0
+        body = json.loads(result.text)
+        return body["data"]["subscribers"]
